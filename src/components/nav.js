41e511692f45037403cc2424eae4a6ef1351c52a
kesavamuthu <kesavamuthu77@gmail.com>
2020-08-09 07:43:02 +0530

    logo added

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,13 @@ function Navigation() {
   return (
     <>
       <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Weather Report</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            style={{ width: "40px" }}
+            src="https://i.pinimg.com/originals/7f/f1/55/7ff155cc19ba480c42c6e455f1ccb982.jpg"
+            alt="logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
